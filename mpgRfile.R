ceca69834f48f2d87896991240d94ec335cad177
Dinosauce343 <98054407+Dinosauce343@users.noreply.github.com>
2024-03-26 21:43:20 -0400

    Update mpgRfile.R
    The data source is the mpg data table

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,11 @@
-# J Dickens  James Dickens
+# Gabriel Fernandes
 
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+#The data source is the mpg data table
+
+ggplot(mpg, aes(x = "", y = displ)) +
+  geom_boxplot(fill = "blue", width = 0.5) +
+  coord_flip()
